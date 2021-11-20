a3c196fb9cb5b63a863a06197581dc2f0dc22ed8
Johnny Briones <jbrrii91@gmail.com>
2021-11-19 21:27:57 -0500

    Added question and answer to screen

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import QuizQuestion from "./QuizQuestion";
 
-let quizData = require('./quiz_data.json')
+let quizData = require("./quiz_data.json");
 
-
-class Quiz extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = ({
-            quiz_position: 1
-        })
-    }
-    render() {
-        return <div> 
-            <div className='QuizQuestion'>
-                {quizData.quiz_questions[0]
-                .instruction_text}
-            </div>
-            
-             </div>
-
-    }
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quiz_position: 1,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div className="QuizQuestion">
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Quiz
+export default Quiz;
